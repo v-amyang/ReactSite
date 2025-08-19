ac596474505c9557454fd57c54d1c311adb1fcd9
v-amyang <v-amyang@microsoft.com>
2025-08-19 02:25:31 +0000

    Add api code

--- app/src/index.js
+++ app/src/index.js
@@ -18,7 +18,7 @@ class FetchDemo extends React.Component {
       axios.get(`/api/${this.props.subreddit}`)
         .then(res => {
           console.log(res.data);
-          var userName = 'Hey ' + res.data.userDetails + '! This is from the Azure Tools for VS Code team!';
+          var userName = 'Hey ' + res.data.userDetails + '! test test This is from the Azure Tools for VS Code team!';
           this.setState({userName});
         });
     }
